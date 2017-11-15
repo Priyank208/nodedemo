00a3ba068919bec21ab42bd7585e6e66f403a880
Priyank208 <priyank208@gmail.com>
2017-11-15 13:21:33 +0530

    First demo on github
    crud of users using json files.

---
+++ index.js
@@ -0,0 +1,90 @@
+var app  = require('express')(),
+    bodyParser = require('body-parser'),
+    fs = require('fs'); // require for read/write file
+
+app.use( bodyParser.json() );  // to support JSON-encoded bodies
+
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+  extended: true
+}));
+
+var users = '';
+
+fs.readFile('user.json', (err, data) => {  
+    if (err) throw err;
+    users = JSON.parse(data);
+});
+
+
+// View all user data
+app.get('/users',function(req,res){
+    return res.send({"status" : "Success","data" : users});  
+});
+
+// View single user data
+app.get('/users/:id',function(req,res){
+    var userData = users.find(item => item.id === parseInt(req.params.id))
+    if(!userData){
+      return res.send({"status" : "User Not found"});
+    }else{
+      return res.send({"status" : "Success","data" : userData});  
+    }    
+});
+
+// Delete user
+app.get('/users/delete/:id',function(req,res){
+
+    let id  = parseInt(req.params.id);
+    users.find(function(item, i){      
+      if(item.id === id){
+        users.splice(i,1);
+        var json = JSON.stringify(users);
+        fs.writeFile("user.json", json, function(err) {
+            if(err) {
+                return res.send({"status" : "Fail","error" : err });  
+            }else{
+              return res.send({"status" : "Success","data" : users });  
+            }
+        });
+      }
+    });   
+});
+
+// Edit user
+app.post('/users/edit/:id',function(req,res){
+    let id  = parseInt(req.params.id);    
+    users.find(function(item, i){      
+      if(item.id === id){
+          item.name = req.body.name;
+          item.username = req.body.username;
+          item.email = req.body.email;
+      }
+    });
+    var json = JSON.stringify(users);
+    fs.writeFile("user.json", json, function(err) {
+        if(err) {
+            return res.send({"status" : "Fail","error" : err });  
+        }else{
+          return res.send({"status" : "Success","data" : users });  
+        }
+    });    
+});
+
+// Add new user
+app.post('/users/add',function(req,res){    
+    let newid = users.length  + parseInt(1);
+    users.push({"id":newid,"name":req.body.name,"username":req.body.username,"email":req.body.email});    
+    var json = JSON.stringify(users);
+    fs.writeFile("user.json", json, function(err) {
+        if(err) {
+            return res.send({"status" : "Fail","error" : err });  
+        }else{
+          return res.send({"status" : "Success","data" : users });  
+        }
+    });
+});
+
+//start Server
+var server = app.listen(3000,function(){
+   console.log("Listening to port %s",server.address().port);
+});
